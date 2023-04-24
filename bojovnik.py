b114c1c7ecc7c15e604875ef0f8153e37b1c0b42
Wochozka <david@svarc.it>
2023-04-24 15:56:19 +0200

    pridano nazivu, priste dlouhy souboj

--- bojovnik.py
+++ bojovnik.py
@@ -10,17 +10,48 @@ class Bojovnik:
     def __init__(self, jmeno, zivot, utok, obrana, kostka):
         self.__jmeno = jmeno
         self.__zivot = zivot
+        self.__maxZivot = zivot
         self.__utok = utok
         self.__obrana = obrana
         self.__kostka = kostka
 
     def __str__(self):
         return f"Bojovnik jmenem {self.__jmeno}."
+    
+    def utok(self, souper):
+        uder = self.__utok + self.__kostka.hod()
+        souper.obrana(uder)
+    
+    def obrana(self, uder):
+        obrana = self.__obrana + self.__kostka.hod()
+        zraneni = uder - obrana
+        if zraneni < 0:
+            zraneni = 0
+        self.__zivot = self.__zivot - zraneni
+    
+    def grafickyZivot(self):
+        celkem = 20
+        pocet = int(self.__zivot / self.__maxZivot * celkem)
+        return f"[{'#'*pocet}{' '*(celkem-pocet)}]"
+    
+    @property
+    def nazivu(self):
+        return self.__zivot > 0
+
 
 def main():
     k = kostka.Kostka(10)
-    b = Bojovnik("Denisa", 100, 40, 30, k)
-    print(b)
+    denisa = Bojovnik("Denisa", 100, 40, 30, k)
+    prvnic = Bojovnik("1c", 120, 50, 35, k)
+    
+    denisa.utok(prvnic)
+    print(prvnic, prvnic.grafickyZivot())
+
+    prvnic.utok(denisa)
+    print(denisa, denisa.grafickyZivot())
+
+    denisa.utok(prvnic)
+    print(prvnic, prvnic.grafickyZivot())
 
 if __name__ == "__main__":
   main()
